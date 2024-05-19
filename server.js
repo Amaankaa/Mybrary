191868b168ff9340623290e3309cfb082a778840
Amaankaa <162874730+Amaankaa@users.noreply.github.com>
2024-05-19 16:13:52 +0300

    Initial Author Routes

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
-if(process.env.NODE_ENV !== 'production') {
-  require('dotenv').parse();
-}
-
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+//Smart move amaan
+const mongo = require('./hide');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended:false }))
 
-const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
-const db = mongoose.connection;
-db.on('error', err => console.log(err));
-db.once('open', () => console.log('Connected to Mongoose'));
 
+mongoose.connect(mongo)
+  .then(() => {
+    console.log('Successfully connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+  
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 app.listen(process.env.PORT || 3000);
